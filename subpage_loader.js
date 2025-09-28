072bc9b345c866254d9486b6270cc9d2a6604537
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2025-09-28 14:30:24 +0500

    Initial commit

---
+++ subpage_loader.js
@@ -0,0 +1,21 @@
+﻿
+for (const el of document.querySelectorAll('[data-include]')) {
+  const file = el.getAttribute('data-include')
+  try {
+    await fetch(file)
+      .then(r => r.text())
+      .then(html => el.innerHTML = html);
+  } catch (type_error) {
+    console.log("Test")
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        el.innerHTML = this.responseText;
+      }
+    };
+
+    xhr.open('GET', file, true);
+    xhr.send();
+  }
+}
